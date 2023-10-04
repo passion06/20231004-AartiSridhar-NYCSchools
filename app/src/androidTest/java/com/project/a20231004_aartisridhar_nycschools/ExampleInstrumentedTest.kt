937d0afa7ec1b3062fd68973e373d5894418205c
Aarti Sridhar <aartisridhar5@gmail.com>
2023-10-04 12:08:19 -0700

    Initial commit after adding dependencies and creating the project structure

---
+++ app/src/androidTest/java/com/project/a20231004_aartisridhar_nycschools/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.project.a20231004_aartisridhar_nycschools
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.project.a20231004_aartisridhar_nycschools", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/project/a20231004_aartisridhar_nycschools/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.project.a20231004_aartisridhar_nycschools
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            }
+        }
+}
+
